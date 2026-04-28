08fbf190dae94075f2cd6617ef8ba1eb8924913d
Aditya <akash.aditya001@gmail.com>
2026-04-28 11:59:38 +0530

    first commit

---
+++ src/components/navbar/MainNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mainnav = () => {
+  return (
+    <div>Mainnav</div>
+  )
+}
+
+export default Mainnav
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Mainnav from './Mainnav'
+import Topbar from './Topbar'
+
+const Navbar = () => {
+  return (
+    <>
+    <Topbar/>
+    <Mainnav/>
+    </>
+  )
+}
+
+export default Navbar

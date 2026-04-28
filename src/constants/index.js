8bdee42599340da2f3e8999a3f1f38c85a32a19a
Aditya <akash.aditya001@gmail.com>
2026-04-29 00:08:28 +0530

    const of navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,13 @@
 export * from './topbar'
+export * from './navbar'
+import arrow from "@/assets/images/common/btn-arrow.svg"
+
+
+export const button = [
+    {
+        lable: "Book Appointment",
+        href: "/",
+        arrowBtn: arrow, 
+        buttonClass: "w-[262px] h-[65px] rounded-full"
+    }
+]
---
+++ src/constants/navbar.js
@@ -0,0 +1,35 @@
+import logo from "@/assets/images/common/brand-logo.svg";
+
+export const silvavmedLogo = [
+    {
+        img: logo,
+    }
+]
+
+export const navLinks = [
+    {
+        id: 1,
+        label: "Home",
+        href: "/"
+    },
+    {
+        id: 2,
+        label: "About Us",
+        href: "/"
+    },
+    {
+        id: 3,
+        label: "Treatments",
+        href: "/"
+    },
+    {
+        id: 4,
+        label: "News",
+        href: "/"
+    },
+    {
+        id: 5,
+        label: "Contact",
+        href: "/"
+    },
+]
